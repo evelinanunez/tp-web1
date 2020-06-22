97101eff1970e2af24d4a77a81a2bab0bbcdffca
Julieta Delfino <JulietaLuciaDelfino@hotmail.com>
2020-06-21 21:38:03 -0300

    Cambios sobre el PopUp (Aun no funciona, se le envio preguntas a la profesora).

---
+++ js/efectos.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#abrir").click (function(){
+        $(".popup").show();
+    });
+    $("#cerrar").click (function(){
+        $(".popup").hide();
+    });
+});
